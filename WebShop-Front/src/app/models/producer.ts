031716bd60266fc41107255adf610136ff853784
Milovan Srejić <srejicmilovan@gmail.com>
2020-08-14 15:53:07 +0200

    Obrada sortiranja i filtracije proizvoda

--- WebShop-Front/src/app/models/producer.ts
+++ WebShop-Front/src/app/models/producer.ts
@@ -1,4 +1,5 @@
 export class Producer {
     public id: number;
     public producerName: string;
+    public isCheck: boolean;
 }
--- WebShop-Front/src/app/pages/products-page/filters/filters.component.ts
+++ WebShop-Front/src/app/pages/products-page/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Producer } from 'src/app/models/producer';
 
 @Component({
@@ -8,6 +8,11 @@ import { Producer } from 'src/app/models/producer';
 })
 export class FiltersComponent implements OnInit {
   @Input() producer: Producer;
+  @Output() onClick: EventEmitter<any> = new EventEmitter();
+
+  onClickProducer(producerId: number) : void {
+     this.onClick.emit(producerId);
+  }
   constructor() { }
 
   ngOnInit(): void {
