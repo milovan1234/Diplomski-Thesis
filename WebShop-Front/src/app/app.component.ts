26c639e00f0019380f9d09ead481ef3724115970
Milovan Srejić <srejicmilovan@gmail.com>
2020-08-28 22:11:19 +0200

    Izmena na glavnoj App komponenti pri ucitavanju tooltip-a

--- WebShop-Front/src/app/app.component.ts
+++ WebShop-Front/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Početna';
+  
+  ngOnInit(): void {
+    this.loadTooltip();
+  }
+
+
+  loadTooltip(): void {
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    });
+  }
+
 }
